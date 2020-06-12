dc70eda7b2067c3b9232bb1eff328cd78b276f96
Jooui <57446238+Jooui@users.noreply.github.com>
2020-06-12 16:18:34 +0200

    Delete apikeys.php

--- client/backend/utils/apikeys.php
+++
@@ -1,3 +0,0 @@
-<?php
-$APIKEY_MAILGUN='0710b92ca98f6d05f7a303187ea4d1dc-915161b7-a0483277';
-$APIURL_MAILGUN='https://api.mailgun.net/v3/sandboxa35ae1383d8f4bd8aff3011ad8b69de2.mailgun.org/messages';
